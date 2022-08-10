7ad14f5d0516c9ab884e547dd26aa490a80b70d7
Emanuelle-02 <85840803+Emanuelle-02@users.noreply.github.com>
2022-08-10 15:32:29 -0300

    Update para abrir o resultado da busca em uma nova guia

--- busca.php
+++ busca.php
@@ -1,3 +1,6 @@
+<?php
+    error_reporting(0);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,18 +10,18 @@
     <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
         name="viewport">
 
-    <link href="css/busca.css" rel="stylesheet" type="text/css">
+    <link href="css/index.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <main class="container">
         <h1 class="title">Consumindo API NADIC COM PHP</h1>
-        <form class="form" action="busca.php" method="POST">
+        <form class="form" action="resultado.php" target="blank" method="POST">
             <div class="row">
                 <div>
                     <h1>Insira a data a ser buscada:</h1>
                     <input type="date" name="dataproposta" class="form___data" <?php echo date('Y-m-d'); ?>>
-                    <span class="form___requirement">* Selecione o período desejado.</span>
+                    <span class="form___requirement">* Selecione ou insira um  a data.</span>
                 </div>
             </div>
             <div class="row has-alignCenter">
@@ -29,37 +32,3 @@
 </body>
 
 </html>
-
-<?php
-$db = pg_connect("host = localhost port = 5432 dbname = dou user = postgres password = postgres");
-$query = "SELECT * FROM licitacao where dataproposta::date = '$_POST[dataproposta]'";
-$result = pg_query($db, $query);
-//$row = pg_fetch_assoc($result);
-if (isset($_POST['submit']))
-{
-    while($row = pg_fetch_assoc($result)){
-        echo "<br>Órgao: <br>";
-        echo $row['orgao'];
-        echo "<br>Título: <br>";
-        echo $row['titulo'];
-        echo "<br>Estado: <br>";
-        echo $row['estado'];
-        echo "<br>Cidade: <br> - ";
-        echo $row['cidade'];
-        echo "<br>Objeto: <br>";
-        echo $row['objeto'];
-        echo "<br>Datas: <br>";
-        echo $row['dataproposta'];
-        echo "<br>";
-        echo $row['datasessao'];
-        echo "<br>";
-    }
-}
-else {
-    if(!$db) {
-        echo "Ocorreu um erro.\n";
-        exit;
-    }
-    pg_close($db);
-}
-?>
